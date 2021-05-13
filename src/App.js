71f3101eccb8e61ffdc6a72ed0320962d16c0eb8
Nathan <nathanbenson.affiliate@gmail.com>
2021-05-13 19:22:33 +0200

    instaled react-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from "react";
-import "./app.scss";
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import './App.scss';
+import { motion } from 'framer-motion';
 
 function App() {
   return (
     <div className="App">
-      <p className="ptag">hello</p>
-      
+      <motion.div animate={{ rotate: 360 }}
+    transition={{ duration: 2 }}>
+        <h1>Hello</h1>
+      </motion.div>
     </div>
   );
 }
